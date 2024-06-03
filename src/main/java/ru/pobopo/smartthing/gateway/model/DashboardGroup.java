37cd0b57eb82ff762056b0e22e0056d96aebb4cf
PavelProjects <paveloalexeev52@gmail.com>
2024-06-03 19:57:56 +0300

    removed builder from DashboardGroup

--- src/main/java/ru/pobopo/smartthing/gateway/model/DashboardGroup.java
+++ src/main/java/ru/pobopo/smartthing/gateway/model/DashboardGroup.java
@@ -9,12 +9,11 @@ import ru.pobopo.smartthing.model.DeviceInfo;
 import java.util.*;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class DashboardGroup {
     private UUID id;
     private DeviceInfo device;
-    private List<DashboardObservable> observables;
-    private DashboardGroupConfig config;
+    private List<DashboardObservable> observables = new ArrayList<>();
+    private DashboardGroupConfig config = new DashboardGroupConfig();
 }
